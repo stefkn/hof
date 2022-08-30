c0db78f3b08ac5b767a52240a11ad96e8204ba0e
Stefan Nowak <stefan.nowak@tails.com>
2022-08-30 10:47:35 +0100

    Update title and webmanifest

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Index({ allPosts: initialAllPosts, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>House of Focus</title>
         </Head>
         <Container>
           <Intro />
