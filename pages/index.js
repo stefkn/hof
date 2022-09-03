d418d7ca03a528f1f8231304fdec5b1bf0557bbc
Stefan Nowak <stefan.nowak@tails.com>
2022-09-03 12:41:20 +0100

    import and use countdown component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
+import CountdownApp from '../components/countdown'
 import { CMS_NAME } from '../lib/constants'
 import { indexQuery } from '../lib/queries'
 import { usePreviewSubscription } from '../lib/sanity'
@@ -23,6 +24,7 @@ export default function Index({ allPosts: initialAllPosts, preview }) {
         </Head>
         <Container>
           <Intro />
+          <CountdownApp />
           {heroPost && (
             <HeroPost
               title={heroPost.title}
