6fab75dee6c3fb4ecd843515adfe745191330cf2
Stefan Nowak <stefan.nowak@tails.com>
2022-09-09 18:53:22 +0100

    make intro logo a link back to index

--- components/intro.js
+++ components/intro.js
@@ -1,10 +1,13 @@
 import { CMS_NAME, CMS_URL } from '../lib/constants'
+import Link from 'next/link'
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 font-serif">
-        house of focus
+        <Link href={`/`}>
+          <a className="hover:underline">house of focus</a>
+        </Link>
       </h1>
       <h4 className="text-right md:text-right text-lg mt-5 md:pl-8 font-sans">
         Live right in your mind.
