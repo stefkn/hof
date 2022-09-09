3df3395ee8b2e4f9362ec7939547342a5ac8ba22
Stefan Nowak <stefan.nowak@tails.com>
2022-09-09 18:58:02 +0100

    remove more useless stuff

--- pages/index.js
+++ pages/index.js
@@ -49,12 +49,3 @@ export default function Index() {
     </>
   )
 }
-
-export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
-  return {
-    props: { allPosts, preview },
-    // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
-    revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
-  }
-}
