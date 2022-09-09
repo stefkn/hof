f85a65f9e96018b619d7a426519c5a8edf56734c
Stefan Nowak <stefan.nowak@tails.com>
2022-09-09 18:54:58 +0100

    remove unused stuff from focus page

--- pages/focus.js
+++ pages/focus.js
@@ -1,24 +1,13 @@
 import Head from 'next/head'
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import CountdownApp from '../components/countdown'
-import { CMS_NAME } from '../lib/constants'
-import { indexQuery } from '../lib/queries'
-import { usePreviewSubscription } from '../lib/sanity'
-import { getClient, overlayDrafts } from '../lib/sanity.server'
 
-export default function Index({ allPosts: initialAllPosts, preview }) {
-  const { data: allPosts } = usePreviewSubscription(indexQuery, {
-    initialData: initialAllPosts,
-    enabled: preview,
-  })
-  const [heroPost, ...morePosts] = allPosts || []
+export default function Index() {
   return (
     <>
-      <Layout preview={preview}>
+      <Layout>
         <Head>
           <title>house of focus</title>
         </Head>
@@ -32,11 +21,3 @@ export default function Index({ allPosts: initialAllPosts, preview }) {
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
-  return {
-    props: { allPosts, preview },
-    // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
-    revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
-  }
-}
