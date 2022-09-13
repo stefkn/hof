fc742829b08e472a9c9e2387119ee0ade0bf7e27
Stefan Nowak <stefan.nowak@tails.com>
2022-09-13 22:57:11 +0100

    remove border on alert

--- components/alert.js
+++ components/alert.js
@@ -5,7 +5,7 @@ import { EXAMPLE_PATH } from '../lib/constants'
 export default function Alert({ preview }) {
   return (
     <div
-      className={cn('border-b', {
+      className={cn('border-0', {
         'bg-accent-7 border-accent-7 text-white': preview,
         'bg-accent-1 border-accent-2': !preview,
       })}
