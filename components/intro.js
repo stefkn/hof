9ddb72d7898e3334afcf9874a230d0bf1948267a
Stefan Nowak <stefan.nowak@tails.com>
2022-09-15 09:25:55 +0100

    update intro component

--- components/intro.js
+++ components/intro.js
@@ -1,15 +1,39 @@
-import { CMS_NAME, CMS_URL } from '../lib/constants'
 import Link from 'next/link'
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 font-serif">
+    <section className="
+    -z-20
+    fixed rotate-[-90deg] top-[7rem] left-[-5rem]
+    flex-col flex items-left
+    md:relative md:flex-row md:justify-between md:top-0 md:left-0 md:rotate-0 md:mt-16 md:mb-16 md:mb-12">
+      <h1 className="
+        text-6xl
+        font-serif
+        font-bold
+        tracking-tighter
+        leading-tight
+
+        md:text-8xl
+        md:pr-8
+      ">
         <Link href={`/`}>
-          <a className="hover:underline">house of focus</a>
+          <a className="hover:text-teal-500">
+            house of focus
+          </a>
         </Link>
       </h1>
-      <h4 className="text-right md:text-right text-lg mt-5 md:pl-8 font-sans">
+      <h4 className="
+        text-left
+        font-sans
+        text-md
+        mt-0
+
+        md:text-right
+        md:pl-8
+        md:mt-20
+        z-10
+      ">
         Live right in your mind.
       </h4>
     </section>
