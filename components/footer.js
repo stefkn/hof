8ef4a6ffa85dc6845f245ffc0efbaef5f695a9a2
Stefan Nowak <stefan.nowak@tails.com>
2022-09-15 09:27:36 +0100

    pull content out of footer bg for stacking

--- components/footer.js
+++ components/footer.js
@@ -3,19 +3,20 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2 bg-slate-900">
+    <footer className="border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-        <h2 className="no-text-shadow mb-10 text-6xl md:text-7xl font-bold tracking-tighter leading-tight font-serif text-white">
-          <Link href="/">
-            <a className="hover:underline">house of focus</a>
-          </Link>
-        </h2>
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/ text-white">
-          your oasis in a world of distraction.
-        </div>
+        <div className="mt-96 ml-1/2 flex flex-col lg:flex-row items-center absolute z-2">
+          <h2 className="no-text-shadow mb-10 text-6xl md:text-7xl font-bold tracking-tighter leading-tight font-serif text-white">
+            <Link href="/">
+              <a className="hover:text-teal-500">house of focus</a>
+            </Link>
+          </h2>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/ text-white">
+            your oasis in a world of distraction.
+          </div>
         </div>
       </Container>
+      <div className="bg-slate-900 w-full h-screen z-[-1] relative"></div>
     </footer>
   )
 }
