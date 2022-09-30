b3dda4b5043fb6a258bf230ee01fdc5c0735148a
Stefan Nowak <stefan.nowak@tails.com>
2022-09-30 15:27:33 +0100

    add backgroundImage as a common component at _app level to prevent rerenders

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/index.css'
+import BackgroundImage from '../components/bg-image'
+// import App from 'next/app'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <BackgroundImage />
+    <Component {...pageProps} />
+    </>
+  )
 }
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
 
 export default MyApp
