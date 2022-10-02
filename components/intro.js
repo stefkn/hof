204972d6e12b2b63b79cd50965a24d17a7c8ed99
Stefan Nowak <stefan.nowak@tails.com>
2022-10-02 23:32:41 +0100

    add id to main logo

--- components/intro.js
+++ components/intro.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Intro() {
   return (
-    <section className="
+    <section id="logo" className="
       fixed rotate-[-90deg] top-[7rem] left-[-5rem]
       flex-col flex items-left
       md:relative md:flex-row md:justify-between md:top-0 md:left-0 md:rotate-0 md:mt-16 md:mb-16 md:mb-12
