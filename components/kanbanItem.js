36d74a1134c00a7c3e1a17757ee63c0cd47da8ea
stefkn <stefan.nowak@tails.com>
2024-05-25 06:46:51 +0100

    refactor: Improve KanbanItem component structure and styles

--- components/kanbanItem.js
+++ components/kanbanItem.js
@@ -19,6 +19,28 @@ export default function KanbanItem({ task, kanbanItemMethods, style }) {
     }
     return (
         <div className={isDraggable ? "" : "no-drag"} draggable={isDraggable ? "true" : "false"}>
+            <div
+                className={"block max-w-2xl p-6 mb-2 bg-white border overflow-hidden border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 " + (styleConfig[style] || "")}
+                data-label={task.title}
+            >
+                {isEditable ? (
+                    <input defaultValue={task.title} type="text" id="title" className="block w-full p-2 mb-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        onChange={(e) => { kanbanItemMethods.updateItem(task.id, e.target.value, task.description)() }}></input>
+                ) : (
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {task.title}
+                    </h5>
+                )}
+                {isEditable ? (
+                    <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        defaultValue={task.description}
+                        onChange={(e) => { kanbanItemMethods.updateItem(task.id, task.title, e.target.value)() }}></textarea>
+                ) : (
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        {task.description}
+                    </p>
+                )}
+            </div>
         </div>
     )
 }
