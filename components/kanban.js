ae1ad77b6ea4230ac6487bc6f461746cff9bc09b
stefkn <stefan.nowak@tails.com>
2024-05-25 06:56:58 +0100

    refactor: Update Kanban component to use drag and drop functionality

--- components/kanban.js
+++ components/kanban.js
@@ -13,6 +13,17 @@ export default function Kanban({ kanbanItems }) {
     sortable: true,
     draggable: (el) => !el.classList.contains("no-drag"),
   }
+
+  const [todoList, todos, setTodos] = useDragAndDrop([], config);
+  const [inProgressList, inProgs, setInProgs] = useDragAndDrop([], config);
+  const [doneList, dones, setDones] = useDragAndDrop([], config);
+  const [focusTask, focus, setFocus] = useDragAndDrop([], {
+    ...config,
+    accepts: (_parent, lastParent) => {
+      return focus.length === 0;
+    }
+  });
+
   return (
     <div>
     </div>
