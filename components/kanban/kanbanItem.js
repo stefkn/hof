66f873e31e3ffd6b152b0814ff752d2c61b76fde
stefkn <stefan.nowak@tails.com>
2024-06-07 12:59:53 +0100

    refactor: Add expandedFocusedInFocusMode style to KanbanItem component

--- components/kanban/kanbanItem.js
+++ components/kanban/kanbanItem.js
@@ -19,7 +19,8 @@ export default function KanbanItem({ task, kanbanItemMethods, style }) {
     const styleConfig = {
         done: 'bg-teal-100 dark:bg-teal-800 hover:bg-teal-200 dark:hover:bg-teal-800',
         expanded: 'bg-indigo-100 dark:bg-indigo-900 hover:bg-indigo-200 dark:hover:bg-indigo-800 min-w-full h-full',
-        expandedFocused: 'bg-indigo-100 dark:bg-indigo-900 hover:bg-indigo-200 dark:hover:bg-indigo-800 min-w-full h-full animation-pulse'
+        expandedFocused: 'bg-indigo-100 dark:bg-indigo-900 hover:bg-indigo-200 dark:hover:bg-indigo-800 min-w-full h-full animation-pulse',
+        expandedFocusedInFocusMode: 'bg-indigo-100 dark:bg-indigo-900 hover:bg-indigo-200 dark:hover:bg-indigo-800 min-w-full h-96 animation-pulse'
     }
 
     function toggleOpenItem(id) {
@@ -38,7 +39,7 @@ export default function KanbanItem({ task, kanbanItemMethods, style }) {
         e.preventDefault();
         const title = e.target.title.value;
         const description = e.target.description.value;
-        kanbanItemMethods.updateItem(task.id, title, description)();
+        kanbanItemMethods.updateItem(task.id, title, description);
         toggleOpenItem(task.id)();
     }
 
@@ -53,7 +54,7 @@ export default function KanbanItem({ task, kanbanItemMethods, style }) {
             >
                 {isEditable ? (
                     <input defaultValue={task.title} type="text" id="title" className="block w-full p-2 mb-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        onChange={(e) => { kanbanItemMethods.updateItem(task.id, e.target.value, task.description)() }}></input>
+                        onChange={(e) => { kanbanItemMethods.updateItem(task.id, e.target.value, task.description) }}></input>
                 ) : (
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         {task.title}
@@ -62,7 +63,7 @@ export default function KanbanItem({ task, kanbanItemMethods, style }) {
                 {isEditable ? (
                     <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         defaultValue={task.description}
-                        onChange={(e) => { kanbanItemMethods.updateItem(task.id, task.title, e.target.value)() }}></textarea>
+                        onChange={(e) => { kanbanItemMethods.updateItem(task.id, task.title, e.target.value) }}></textarea>
                 ) : (
                     <p className="font-normal text-gray-700 dark:text-gray-400">
                         {task.description}
